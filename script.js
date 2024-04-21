5f6b04df49deacbb3ce3d15da853c8e08aadd52f
hi-scp <91630921+hi-scp@users.noreply.github.com>
2024-04-20 23:57:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -61,24 +61,23 @@ fetch('personality_questions.txt')
                 localStorage.setItem("personality_result", userResponses);
 
                 
-                var p1 = userResponses.substring(0, 3);
-                var p2 = userResponses.substring(3);
+                var p1 = userResponses.substring(0, 50);
+                var p2 = userResponses.substring(51);
 
 
-                var sum1 = 0;
+                var sum1 = 20;
 
                 for (var i = 0; i < p2.length; i++) {
-                    if (!isNaN(parseInt(p2[i]))) {
-                    // If it's a digit, convert it to a number and add it to the sum
-                    sum1 += parseInt(p2[i]);
+                    d = parseInt(p2[i]);
+                    if ( d%10 == 1) {
+                        sum1 += d;
+                    
+                    } else if ( d%10 == 6) {
+                        sum1 -= d;
+                    }
                 }
-                }
-
-
-
 
                     
-                // E = 20 + (1) _1__ - (6) __4_ + (11) __3_ - (16) _5__ + (21) _4__ - (26) _3__ + (31) _2__ - (36) _5__ + (41) _1__ - (46) _4__ = __10___
                 document.getElementById('yesCount').textContent = sum1;
                     
                 // window.location.href = "index.html";
