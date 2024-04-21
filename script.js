aa133eab252fc8c14fb7c9c5d68e8ac8eb7aa2d4
hi-scp <91630921+hi-scp@users.noreply.github.com>
2024-04-20 23:42:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,29 @@ fetch('personality_questions.txt')
                 }
                 else {
                 localStorage.setItem("personality_result", userResponses);
-                window.location.href = "index.html";
+
+                
+                var p1 = userResponses.substring(0, 50);
+                var p2 = userResponses.substring(50);
+
+
+                var sum1 = 0;
+
+                for (var i = 0; i < p1.length; i++) {
+                    if (!isNaN(parseInt(p1[i]))) {
+                    // If it's a digit, convert it to a number and add it to the sum
+                    sum1 += parseInt(p1[i]);
+                }
+                }
+
+
+
+
+                    
+                // E = 20 + (1) _1__ - (6) __4_ + (11) __3_ - (16) _5__ + (21) _4__ - (26) _3__ + (31) _2__ - (36) _5__ + (41) _1__ - (46) _4__ = __10___
+                document.getElementById('yesCount').textContent = sum1;
+                    
+                // window.location.href = "index.html";
                 }
                 }
             
